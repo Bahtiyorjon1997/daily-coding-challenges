ab82c77ba173dcf6b9806a155a2b8420b06f6365
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2022-04-22 05:49:22 +0200

    sample commit

--- Documents/ReactCloneProjects/online-market-project/src/components/Home.js
+++ Documents/ReactCloneProjects/online-market-project/src/components/Home.js
@@ -29,12 +29,7 @@ const Home = () => {
               <span className="subtitle">Men</span>
             </div>
           </div>
-          <div className="menu-item">
-            <div className="content">
-              <h1 className="title">Hats</h1>
-              <span className="subtitle">Men</span>
-            </div>
-          </div>
+          
         </div>
       </div>
     );
